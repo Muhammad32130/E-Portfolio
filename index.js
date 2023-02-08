1bd8433665d237d79fbbb546b5c4e53cead2605f
Muhammad32130 <askari.muhammad321@gmail.com>
2023-02-08 12:59:59 -0600

    add

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-// // template_9c9svdw
-// // afAh1yi6uFuDhPzP1
+// template_9c9svdw
+// afAh1yi6uFuDhPzP1
 
 
 
-// function showInfo(){
+function showInfo(){
     
-//     document.body.classList+=(' open--card')
+    document.body.classList+=(' open--card')
+}
+function closeInfo(){
+    setTimeout(function(){
+        document.body.classList.remove('open--card')
+
+    },100)
+}
+function contact(event){
+    event.preventDefault();
+    const loading = document.querySelector(' .loading ')
+    const success = document.querySelector(' .success ')
+    loading.classList += " visible"
+    emailjs
+        .sendForm(
+            `service_4etywk5`,
+            `template_9c9svdw`,
+            event.target,
+            `afAh1yi6uFuDhPzP1`
+        ).then(()=>{
+            loading.classList.remove("visible")
+            success.classList += " visible"
+        }).catch(()=>{
+            loading.classList.remove("visible")
+            alert(
+                'The email service is temporarily unavalable. Please contact me directly on askari.muhammad321@gmail.com'
+            )
+        })
+
+
+}
+// const menu = document.querySelector(' .anchor__contain')
+// function showMenuonClick(){
+//     menu.classList += " showmenu"
 // }
-// function closeInfo(){
-//     setTimeout(function(){
-//         document.body.classList.remove('open--card')
-
-//     },100)
+// function closemenu(){
+//     menu.classList.remove("showmenu")
 // }
-// function contact(event){
-//     event.preventDefault();
-//     const loading = document.querySelector(' .loading ')
-//     const success = document.querySelector(' .success ')
-//     loading.classList += " visible"
-//     emailjs
-//         .sendForm(
-//             `service_4etywk5`,
-//             `template_9c9svdw`,
-//             event.target,
-//             `afAh1yi6uFuDhPzP1`
-//         ).then(()=>{
-//             loading.classList.remove("visible")
-//             success.classList += " visible"
-//         }).catch(()=>{
-//             loading.classList.remove("visible")
-//             alert(
-//                 'The email service is temporarily unavalable. Please contact me directly on askari.muhammad321@gmail.com'
-//             )
-//         })
+    
+    
+    
+    
+    window.addEventListener('scroll',
+    ()=>{
+        const project = document.querySelector('.project')
+        let contentposition = project.getBoundingClientRect().top
+        let screenPosition = window.innerHeight 
 
-
-// }
-// // const menu = document.querySelector(' .anchor__contain')
-// // function showMenuonClick(){
-// //     menu.classList += " showmenu"
-// // }
-// // function closemenu(){
-// //     menu.classList.remove("showmenu")
-// // }
-    
-    
-    
-    
-//     window.addEventListener('scroll',
-//     ()=>{
-//         const project = document.querySelector('.project')
-//         let contentposition = project.getBoundingClientRect().top
-//         let screenPosition = window.innerHeight 
-
-//         if(contentposition<screenPosition){
-//             project.classList.add('show__project')
-//         }
-//         else{
-//             project.classList.remove('show__project')
-//         }
-//     }
-//     )
+        if(contentposition<screenPosition){
+            project.classList.add('show__project')
+        }
+        else{
+            project.classList.remove('show__project')
+        }
+    }
+    )
     
     
 
